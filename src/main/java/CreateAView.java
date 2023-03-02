0437273e0f1f54ad07fcd7a679bf7cab82dfeb43
leestephen0320 <leestephen0320@gmail.com>
2023-03-02 21:43:46 +0000

    modified:   problem1.sql

--- src/main/java/CreateAView.java
+++ src/main/java/CreateAView.java
@@ -66,7 +66,7 @@ public class CreateAView {
 
 
     public void problem1(){
-        /**
+        /** 
          * problem1: Create a view called "firstname_lastname" in problem1.sql from the site_user table that only has the firstname and lastname columns.
          * NOTE: This table should NOT have the id and age.
          */
